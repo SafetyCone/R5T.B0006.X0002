6ea02062655d359cd8e677abcaa6d3e82c92fa8a
davidcoats <david@getsafetycone.com>
2022-05-07 23:26:27 -0700

    Initial commit of source-stub.

---
+++ source/R5T.B0006.X0002.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.B0006.X0002.Construction
+{
+    /// <summary>
+    /// Construction project for Generation, initial, parse, simple extensions to ISyntaxGenerator..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.B0006.X0002/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.B0006.X0002
+{
+    /// <summary>
+    /// Generation, initial, parse, simple extensions to ISyntaxGenerator.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
